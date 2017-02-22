4fe09571e24412fcbeba2cc14ed9092433cb4da6
Lauren Arfaras <larfaras96@gmail.com>
2017-02-22 00:20:54 -0500

    made the directory availible to all files in the index.js file

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var express = require('express');
 var server = express();
 var port = process.env.PORT || 8080;
 
-server.use(express.static(__dirname + '/public'));
+server.use(express.static(__dirname + '/html5up-stellar'));
 
 server.get('/', function(request, response){
-  response.sendFile('index.html', {root: __dirname + '/public/html'});
+  response.sendFile('index.html', {root: __dirname + '/html5up-stellar'});
 });
 
 server.listen(port, function(){
